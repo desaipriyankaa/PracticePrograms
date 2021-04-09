2d478538920d55203634fac282a98b1f5f068e37
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-04-09 10:59:03 +0530

    Add project files.

---
+++ ArrayAddition/ArrayCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ArrayAddition
+{
+    class ArrayCalculator
+    {
+        public int addAll(List<int> numbers)
+        {
+            int num = 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                num += numbers[i];
+            }
+            return num;
+        }
+
+        public int addEven(List<int> numbers)
+        {
+            int eresult = 0;
+            for (int i = 0; i < numbers.Count; i += 2)
+            {
+                eresult += numbers[i];
+            }
+            return eresult;
+        }
+        public int addOdd(List<int> numbers)
+        {
+            int oresult = 0;
+            for (int i = 1; i < numbers.Count; i += 2)
+            {
+                oresult += numbers[i];
+            }
+            return oresult;
+        }
+    }
+}
---
+++ ArrayAddition/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayAddition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                num = Int32.Parse(args[i]);
+                numbers.Add(num);
+                Console.WriteLine(" Numbers are : " + num);
+            }
+
+            ArrayCalculator t = new ArrayCalculator();
+            Console.WriteLine();
+
+            while (true)
+            {
+                Console.WriteLine("Enter your choice :- ");
+                Console.WriteLine(" 1.Addition of all numbers\n 2.Addition of numbers at Even position\n 3.Addition of numbers at Odd position\n 4.Exit");
+                try
+                {
+                    int choice = int.Parse(Console.ReadLine());
+
+                    if (choice == 4)
+                    {
+                        break;
+                    }
+
+                    switch (choice)
+                    {
+                        case 1:
+                            int adda = t.addAll(numbers);
+                            Console.WriteLine(adda);
+                            break;
+
+                        case 2:
+                            int adde = t.addEven(numbers);
+                            Console.WriteLine(adde);
+                            break;
+
+                        case 3:
+                            int addo = t.addOdd(numbers);
+                            Console.WriteLine(addo);
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid Choice..... Please select\n1.Addition of all numbers\n 2.Addition of numbers at Even position\n 3.Addition of numbers at Odd position\n 4.Exit");
+                            break;
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+
+            }
+        }
+    }
+}
