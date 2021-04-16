5946dcdf928858bb6b49e583381b0e799e465151
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-04-16 15:55:47 +0530

    switch case outside of try block

--- ArrayAddition/Program.cs
+++ ArrayAddition/Program.cs
@@ -7,6 +7,7 @@ namespace ArrayAddition
     {
         static void Main(string[] args)
         {
+            int choice = 0;
             List<int> numbers = new List<int>();
 
             for (int i = 0; i < args.Length; i++)
@@ -25,34 +26,13 @@ namespace ArrayAddition
                 Console.WriteLine(" 1.Addition of all numbers\n 2.Addition of numbers at Even position\n 3.Addition of numbers at Odd position\n 4.Exit");
                 try
                 {
-                    int choice = int.Parse(Console.ReadLine());
+                    choice = int.Parse(Console.ReadLine());
 
                     if (choice == 4)
                     {
                         break;
                     }
 
-                    switch (choice)
-                    {
-                        case 1:
-                            int adda = t.addAll(numbers);
-                            Console.WriteLine(adda);
-                            break;
-
-                        case 2:
-                            int adde = t.addEven(numbers);
-                            Console.WriteLine(adde);
-                            break;
-
-                        case 3:
-                            int addo = t.addOdd(numbers);
-                            Console.WriteLine(addo);
-                            break;
-
-                        default:
-                            Console.WriteLine("Invalid Choice..... Please select\n1.Addition of all numbers\n 2.Addition of numbers at Even position\n 3.Addition of numbers at Odd position\n 4.Exit");
-                            break;
-                    }
                 }
                 catch (ArgumentException ex)
                 {
@@ -67,6 +47,28 @@ namespace ArrayAddition
                     Console.WriteLine("**");
                 }
 
+
+                switch (choice)
+                {
+                    case 1:
+                        int adda = t.addAll(numbers);
+                        Console.WriteLine(adda);
+                        break;
+
+                    case 2:
+                        int adde = t.addEven(numbers);
+                        Console.WriteLine(adde);
+                        break;
+
+                    case 3:
+                        int addo = t.addOdd(numbers);
+                        Console.WriteLine(addo);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Choice..... Please select\n1.Addition of all numbers\n 2.Addition of numbers at Even position\n 3.Addition of numbers at Odd position\n 4.Exit");
+                        break;
+                }
             }
         }
     }
